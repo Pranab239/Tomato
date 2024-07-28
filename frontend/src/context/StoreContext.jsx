593384f20cf582248d514410fc7872792c283496
Pranab Paul <pranab4752@gmail.com>
2024-07-29 02:29:54 +0530

    addToCart removeFromCart getCart Functionality added

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -10,17 +10,23 @@ const StoreContextProvider = (props) => {
     const [token,setToken] = useState("");
     const [food_list,setFoodList] = useState([]);
 
-    const addToCart = (itemId) => {
+    const addToCart = async (itemId) => {
         if (!cartItems[itemId]) {
             setCartItems((prev)=>({...prev,[itemId]:1}))    // if cart is empty add item with count 1
         }
         else {
             setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))    // if item is already in cart increment count
         }
+        if(token) {
+            await axios.post(url+"/api/cart/add",{itemId},{headers:{token}})    // added to the database also
+        }
     }
 
-    const removeFromCart = (itemId) => {
+    const removeFromCart = async (itemId) => {
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))    // decrement count of item in cart
+        if(token) {
+            await axios.post(url+"/api/cart/remove",{itemId},{headers:{token}}) // remove from the database entry also
+        }
     }
 
     const getTotalCartAmount = () => {
@@ -40,12 +46,18 @@ const StoreContextProvider = (props) => {
         setFoodList(response.data.data)
     }
 
+    const loadCartData = async (token) => {
+        const response = await axios.post(url+"/api/cart/get",{},{headers:{token}});
+        setCartItems(response.data.cartData);
+    }
+
     // Don't logout after refreshing 
     useEffect(()=>{
         async function loadData() {
             await fetchFoodList();
             if(localStorage.getItem("token")) {
-                setToken(localStorage.getItem("token"))
+                setToken(localStorage.getItem("token"));
+                await loadCartData(localStorage.getItem("token")); 
             }
         }
         loadData();
