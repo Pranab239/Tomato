3e8ed50d537a7369c8309ac39fffa3e822fab448
Pranab Paul <pranab4752@gmail.com>
2024-07-29 18:28:42 +0530

    Hide mongodb key

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://pranab4752:tg0aLb3E2Us5TA9K@cluster0.nguxuoq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log("DB Connected"));
+    await mongoose.connect('your_mongodb_key').then(()=>console.log("DB Connected"));
 }
